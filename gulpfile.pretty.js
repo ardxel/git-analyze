5c94c145fe83069584342b2f84b7aaa9c747caad
Vasily Bobnev <ardxel@gmail.com>
2024-11-14 03:30:19 +0300

    update blob sources for prettier

--- gulpfile.pretty.js
+++ gulpfile.pretty.js
@@ -3,11 +3,12 @@ import fs from 'fs';
 import path from 'path';
 import prettier from 'gulp-prettier';
 
+const filesGlob = ['./gulpfile*', './assets/js/*.js', './assets/templates/*.html'];
 const configPath = path.relative(process.cwd(), '.prettierrc');
 const prettierConfig = JSON.parse(fs.readFileSync(configPath, { encoding: 'utf8' }));
 
 const pretty = () => {
-  return gulp.src(['./gulpfile.bundle.js']).pipe(prettier(prettierConfig));
+  return gulp.src(filesGlob).pipe(prettier(prettierConfig));
 };
 
 export default pretty;
